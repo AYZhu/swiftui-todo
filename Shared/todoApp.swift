46c30e61e1e9596041a43855c0c0489e7a01772e
Alan Zhu <alanyzhu@mit.edu>
2022-08-21 14:51:38 -0700

    Initial Commit

---
+++ Shared/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  Shared
+//
+//  Created by Alan Zhu on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
